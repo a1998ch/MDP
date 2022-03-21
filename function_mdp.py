773ef1a3a4fd39b56ceceb6527c9b4339ee1aec6
Александр Чугуевский <93049461+a1998ch@users.noreply.github.com>
2022-03-21 19:06:44 +0700

    готово

--- function_mdp.py
+++ function_mdp.py
@@ -1,4 +1,5 @@
 import sys
+from matplotlib.pyplot import flag
 import pandas as pd
 import win32com.client
 rastr = win32com.client.Dispatch('Astra.Rastr')
@@ -39,6 +40,16 @@ zag_it = vetv.Cols("zag_it")
 zag_i_av = vetv.Cols("zag_i_av")
 zag_it_av = vetv.Cols("zag_it_av")
 
+listic_nodes = []
+slovaric_nodes_index = {}
+index_vetv_sech = []
+index_vozmush = []
+listic_sta_faults_ischodnii = []
+p_sech_nach = []
+
+# Осуществляем расчёт режима.
+kod = rastr.rgm('p')
+
 # Количество узлов и ветвей в заданном режиме
 nodes_count = node.size
 vetv_count = vetv.size
@@ -52,44 +63,125 @@ df_flowgate = pd.DataFrame(flowgate).transpose()
 # Перечень нормативных возмущений
 df_faults = pd.DataFrame(faults).transpose()
 
+
 # Определяем номера узлов в заданном режиме
 # Расчёт тангенса нагрузки
-listic_nodes = []
-for i in range(nodes_count):
-    listic_nodes.append(ny.Z(i))
-    if tip.Z(i) == 1 and pn.Z(i) != 0:
-        tg.SetZ(i, qn.Z(i) / pn.Z(i))
+def nodes_number():
+    """Функция осуществляет подсчёт узлов в
+       заданном режиме, а также производит
+       расчёт тангенса нагрузки
+
+    Аргументы:
+        ny: номер узла.
+        pn: активная мощность нагрузки.
+        qn: реактивная мощность генерации.
+        tip: тип узла.
+        tg: тангенс угла нагрузки
+    Возвращает:
+        список узлов режима.
+    """
+    if len(listic_nodes) == 0:
+        for i in range(nodes_count):
+            listic_nodes.append(ny.Z(i))
+            if tip.Z(i) == 1 and pn.Z(i) != 0:
+                tg.SetZ(i, qn.Z(i) / pn.Z(i))
+        return listic_nodes
+    else:
+        return listic_nodes
+
 
 # Определяем индексы узлов в заданном режиме
-slovaric_nodes_index = {}
-for i, num in enumerate(listic_nodes):
-    slovaric_nodes_index[num] = i
+def nodes_index():
+    """Функция определяет индексы узлов режима
 
-# Переток в сечении в исходном режиме,
-# индексы ветвей в сечении,
-# индексы ветвей, эксплуатационное состояние
-# которых требуется изменить
-# Определение исходного состояния ветвей
-index_vetv_sech = []
-index_vozmush = []
-listic_sta_faults_ischodnii = []
-p_sech_nach = 0
+    Аргументы:
+        nodes_number: Функция, которая осуществляет
+                      подсчёт узлов в заданном режиме.
+    Возвращает:
+        словарь узлов и их индексов режима.
+    """
+    if len(slovaric_nodes_index) == 0:
+        for i, num in enumerate(nodes_number()):
+            slovaric_nodes_index[num] = i
+        return slovaric_nodes_index
+    else:
+        return slovaric_nodes_index
 
-for i in range(vetv_count):
-    for j in range(1, len(df_flowgate.index) + 1):
-        if n_nach.Z(i) == df_flowgate.at[f'line_{j}', 'ip'] and \
-         n_kon.Z(i) == df_flowgate.at[f'line_{j}', 'iq'] and \
-         np.Z(i) == df_flowgate.at[f'line_{j}', 'np']:
-            p_sech_nach += p_nach.Z(i)
-            index_vetv_sech.append(i)
-            j += 1
-    for k in range(len(df_faults.index)):
-        if n_nach.Z(i) == df_faults.iloc[k]['ip'] and \
-         n_kon.Z(i) == df_faults.iloc[k]['iq'] and \
-         np.Z(i) == df_faults.iloc[k]['np']:
-            index_vozmush.append(i)
-            listic_sta_faults_ischodnii.append(sta.Z(i))
-            k += 1
+
+# Определение индексов ветвей в сечении.
+def index_vetv_sech_f():
+    """Функция определяет индексы ветвей в сечении
+
+    Аргументы:
+        n_nach: номер начала ветви.
+        n_kon: номер конца ветви.
+        np: номер параллельности ветви.
+    Возвращает:
+        словарь узлов и их индексов режима.
+    """
+    if len(index_vetv_sech) == 0:
+        for i in range(vetv_count):
+            for j in range(1, len(df_flowgate.index) + 1):
+                if n_nach.Z(i) == df_flowgate.at[f'line_{j}', 'ip'] and \
+                    n_kon.Z(i) == df_flowgate.at[f'line_{j}', 'iq'] and \
+                        np.Z(i) == df_flowgate.at[f'line_{j}', 'np']:
+                    index_vetv_sech.append(i)
+        return index_vetv_sech
+    else:
+        return index_vetv_sech
+
+
+# Определение перетока в сечении в исходном режиме.
+def p_sech_nach_f():
+    """Функция определяет переток в сечении
+       в исходном режиме
+
+    Аргументы:
+        n_nach: номер начала ветви.
+        n_kon: номер конца ветви.
+        np: номер параллельности ветви.
+    Возвращает:
+        переток в сечении в исходном режиме.
+    """
+    if len(p_sech_nach) == 0:
+        p_sech_nach_p = 0
+        for i in range(vetv_count):
+            for j in range(1, len(df_flowgate.index) + 1):
+                if n_nach.Z(i) == df_flowgate.at[f'line_{j}', 'ip'] and \
+                    n_kon.Z(i) == df_flowgate.at[f'line_{j}', 'iq'] and \
+                        np.Z(i) == df_flowgate.at[f'line_{j}', 'np']:
+                    p_sech_nach_p += p_nach.Z(i)
+        p_sech_nach.append(p_sech_nach_p)
+        return p_sech_nach[0]
+    else:
+        return p_sech_nach[0]
+
+
+# Определение индексов ветвей, эксплуатационное состояние
+# которых требуется изменить.
+def index_vozmush_f():
+    """Функция определяет индексы ветвей,
+       которые входят в нормативные возмущения
+
+    Аргументы:
+        n_nach: номер начала ветви.
+        n_kon: номер конца ветви.
+        np: номер параллельности ветви.
+    Возвращает:
+        индексы ветвей, которые входят
+        в нормативные возмущения.
+    """
+    if len(index_vozmush) == 0:
+        for i in range(vetv_count):
+            for k in range(len(df_faults.index)):
+                if n_nach.Z(i) == df_faults.iloc[k]['ip'] and \
+                    n_kon.Z(i) == df_faults.iloc[k]['iq'] and \
+                        np.Z(i) == df_faults.iloc[k]['np']:
+                    index_vozmush.append(i)
+                    listic_sta_faults_ischodnii.append(sta.Z(i))
+        return index_vozmush
+    else:
+        return index_vozmush
 
 
 # Функция утяжеления
@@ -109,17 +201,17 @@ def utyazhelenie():
     for j, i in enumerate(df_vector.node.T.tolist()):
         if df_vector.iloc[j]['variable'] == 'pn':
             pn.SetZ(
-                slovaric_nodes_index.get(i), pn.Z(
-                    slovaric_nodes_index.get(i)) + df_vector.iloc[j]['value'])
+                nodes_index().get(i), pn.Z(
+                    nodes_index().get(i)) + df_vector.iloc[j]['value'])
             if df_vector.iloc[j]['tg'] == 1:
                 qn.SetZ(
-                    slovaric_nodes_index.get(i), tg.Z(
-                        slovaric_nodes_index.get(i)) * pn.Z(
-                            slovaric_nodes_index.get(i)))
+                    nodes_index().get(i), tg.Z(
+                        nodes_index().get(i)) * pn.Z(
+                            nodes_index().get(i)))
         else:
             pg.SetZ(
-                slovaric_nodes_index.get(i), pg.Z(
-                    slovaric_nodes_index.get(i)) + df_vector.iloc[j]['value'])
+                nodes_index().get(i), pg.Z(
+                    nodes_index().get(i)) + df_vector.iloc[j]['value'])
 
 
 # Функция обратного утяжеления
@@ -139,17 +231,17 @@ def obratnoe_utyazhelenie():
     for j, i in enumerate(df_vector.node.T.tolist()):
         if df_vector.iloc[j]['variable'] == 'pn':
             pn.SetZ(
-                slovaric_nodes_index.get(i), pn.Z(
-                    slovaric_nodes_index.get(i)) - df_vector.iloc[j]['value'])
+                nodes_index().get(i), pn.Z(
+                    nodes_index().get(i)) - df_vector.iloc[j]['value'])
             if df_vector.iloc[j]['tg'] == 1:
                 qn.SetZ(
-                    slovaric_nodes_index.get(i), tg.Z(
-                        slovaric_nodes_index.get(i)) * pn.Z(
-                            slovaric_nodes_index.get(i)))
+                    nodes_index().get(i), tg.Z(
+                        nodes_index().get(i)) * pn.Z(
+                            nodes_index().get(i)))
         else:
             pg.SetZ(
-                slovaric_nodes_index.get(i), pg.Z(
-                    slovaric_nodes_index.get(i)) - df_vector.iloc[j]['value'])
+                nodes_index().get(i), pg.Z(
+                    nodes_index().get(i)) - df_vector.iloc[j]['value'])
 
 
 # Функция возврата к исходному режиму
@@ -165,7 +257,7 @@ def vozvrat_k_ishodnomu_regimu():
     """
     for i in range(sys.maxsize):
         kod = rastr.rgm('p')
-        if peretok_v_sechenii() > p_sech_nach:
+        if peretok_v_sechenii() > p_sech_nach_f():
             obratnoe_utyazhelenie()
             kod = rastr.rgm('p')
         else:
@@ -184,6 +276,301 @@ def peretok_v_sechenii():
         Величину перетока в сечении.
     """
     sechenie = 0
-    for j in index_vetv_sech:
+    for j in index_vetv_sech_f():
         sechenie += p_nach.Z(j)
     return sechenie
+
+
+# Определение предельного перетока по критерию обеспечения
+# нормативного коэффициента запаса статической апериодической
+# устойчивости по активной мощности
+# в контролируемом сечении в нормальной схеме.
+def pred_1(utyazhelenie, peretok_v_sechenii, kod):
+    """Функция осуществляет определение предельного перетока
+       по критерию обеспечения нормативного коэффициента
+       запаса статической апериодической устойчивости по
+       активной мощности в контролируемом сечении
+       в нормальной схеме.
+
+    Аргументы:
+        utyazhelenie: осуществляет утяжеление режима
+        peretok_v_sechenii: определяет переток в сечении
+        kod: осуществляет расчёт режима
+    Возвращает:
+        Предельный переток в сечении.
+    """
+    while kod == 0:
+        utyazhelenie()
+        kod = rastr.rgm('p')
+        peretok_v_sechenii()
+    return peretok_v_sechenii()
+
+
+# Определение предельного перетока по критерию
+# обеспечения нормативного коэффициента запаса
+# статической устойчивости по напряжению
+# в узлах нагрузки в нормальной схеме.
+def pred_2(utyazhelenie, peretok_v_sechenii, kod):
+    """Функция осуществляет определение предельного перетока
+       по критерию обеспечения нормативного коэффициента
+       запаса статической устойчивости по напряжению
+       в узлах нагрузки в нормальной схеме.
+
+    Аргументы:
+        utyazhelenie: осуществляет утяжеление режима
+        peretok_v_sechenii: определяет переток в сечении
+        kod: осуществляет расчёт режима
+    Возвращает:
+        Предельный переток в сечении.
+    """
+    while kod == 0:
+        kod = rastr.rgm('p')
+        for j in range(nodes_count):
+            if uras.Z(j) < (unom.Z(j) * 0.7) * 1.15:
+                break
+        if uras.Z(j) > (unom.Z(j) * 0.7) * 1.15:
+            utyazhelenie()
+            kod = rastr.rgm('p')
+        else:
+            peretok_v_sechenii()
+            break
+    else:
+        peretok_v_sechenii()
+    return peretok_v_sechenii()
+
+
+# Определение предельного перетока по критерию обеспечения
+# нормативного коэффициента запаса статической
+# апериодической устойчивости по активной мощности
+# в контролируемом сечении в послеаварийных режимах
+# после нормативных возмущений.
+def pred_3(utyazhelenie, peretok_v_sechenii, kod):
+    """Функция осуществляет определение предельного перетока
+       по критерию обеспечения нормативного коэффициента запаса
+       статической апериодической устойчивости по
+       активной мощности в контролируемом сечении
+       в послеаварийных режимах после нормативных возмущений.
+
+    Аргументы:
+        utyazhelenie: осуществляет утяжеление режима
+        peretok_v_sechenii: определяет переток в сечении
+        kod: осуществляет расчёт режима
+    Возвращает:
+        Предельный переток в сечении.
+    """
+    pred = {}
+    pred_sta = {}
+    index_vozmush_statica = list(index_vozmush_f())
+    listic_sta_faults_statica = df_faults.sta.T.tolist()
+
+    for j in range(len(index_vozmush_statica)):
+        for (index, sta_faults) in zip(
+                index_vozmush_statica, listic_sta_faults_statica):
+            sta.SetZ(index, sta_faults)
+            break
+        index_vozmush_statica.remove(index)
+        listic_sta_faults_statica.remove(sta_faults)
+        while kod == 0:
+            kod = rastr.rgm('p')
+            if kod == 0:
+                utyazhelenie()
+            else:
+                pred[index] = peretok_v_sechenii() * 0.92
+                pred_sta[sta_faults] = peretok_v_sechenii() * 0.92
+                sta.SetZ(index, abs(sta_faults - 1))
+                kod = rastr.rgm('p')
+                break
+        while kod == 0:
+            kod = rastr.rgm('p')
+            if peretok_v_sechenii() > p_sech_nach_f() and \
+                    len(index_vozmush_statica) > 0:
+                obratnoe_utyazhelenie()
+                kod = rastr.rgm('p')
+                summa = 0
+            else:
+                break
+
+    for key, value in pred.items():
+        min_pred_index = min(pred, key=pred.get)
+    min_pred = pred[min_pred_index]
+
+    for key, value in pred_sta.items():
+        min_pred_sta = min(pred_sta, key=pred_sta.get)
+
+    # Повторное разутяжеление до наименьшего
+    # предельного перетока с 8% запасом
+    # из рассматриваемых возмущений
+    kod = rastr.rgm('p')
+    while kod == 0:
+        for i in range(nodes_count):
+            sta.SetZ(min_pred_index, min_pred_sta)
+            break
+        kod = rastr.rgm('p')
+        if peretok_v_sechenii() > min_pred:
+            obratnoe_utyazhelenie()
+            kod = rastr.rgm('p')
+        else:
+            sta.SetZ(min_pred_index, abs(min_pred_sta - 1))
+            rastr.rgm('p')
+            peretok_v_sechenii()
+            break
+    return peretok_v_sechenii()
+
+
+# Определение предельного перетока по критерию
+# обеспечения нормативного коэффициента запаса
+# статической устойчивости по напряжению
+# в узлах нагрузки в послеаварийных режимах
+# после нормативных возмущений.
+def pred_4(utyazhelenie, peretok_v_sechenii, kod):
+    """Функция осуществляет определение предельного перетока
+       по критерию обеспечения нормативного коэффициента
+       запаса статической устойчивости по напряжению
+       в узлах нагрузки в послеаварийных режимах
+       после нормативных возмущений.
+
+    Аргументы:
+        utyazhelenie: осуществляет утяжеление режима
+        peretok_v_sechenii: определяет переток в сечении
+        kod: осуществляет расчёт режима
+    Возвращает:
+        Предельный переток в сечении.
+    """
+    pred = {}
+    index_vozmush_naprygenie = list(index_vozmush_f())
+    listic_sta_faults_naprygenie = df_faults.sta.T.tolist()
+    kod = rastr.rgm('p')
+
+    for j in range(len(index_vozmush_naprygenie)):
+        for (index, sta_faults) in zip(
+                index_vozmush_naprygenie, listic_sta_faults_naprygenie):
+            sta.SetZ(index, sta_faults)
+            break
+        index_vozmush_naprygenie.remove(index)
+        listic_sta_faults_naprygenie.remove(sta_faults)
+        while kod == 0:
+            kod = rastr.rgm('p')
+            for j in range(nodes_count):
+                if uras.Z(j) < (unom.Z(j) * 0.7) * 1.1:
+                    break
+            if uras.Z(j) > (unom.Z(j) * 0.7) * 1.1:
+                utyazhelenie()
+                kod = rastr.rgm('p')
+            else:
+                sta.SetZ(index, abs(sta_faults - 1))
+                kod = rastr.rgm('p')
+                pred[index] = peretok_v_sechenii()
+                peretok_v_sechenii()
+                break
+        else:
+            sta.SetZ(index, abs(sta_faults - 1))
+            kod = rastr.rgm('p')
+            pred[index] = peretok_v_sechenii()
+            peretok_v_sechenii()
+        while kod == 0:
+            kod = rastr.rgm('p')
+            if peretok_v_sechenii() > p_sech_nach_f() and \
+                    len(index_vozmush_naprygenie) > 0:
+                obratnoe_utyazhelenie()
+                kod = rastr.rgm('p')
+            else:
+                break
+
+    for key, value in pred.items():
+        min_pred_index = min(pred, key=pred.get)
+    min_pred = pred[min_pred_index]
+    return min_pred
+
+
+# Определение предельного перетока по критерию обеспечения
+# допустимой токовой нагрузки линий электропередачи и
+# электросетевого оборудования в нормальной схеме.
+def pred_5(utyazhelenie, peretok_v_sechenii, kod):
+    """Функция осуществляет определение предельного перетока
+       по критерию обеспечения допустимой токовой нагрузки
+       линий электропередачи и электросетевого оборудования
+       в нормальной схеме.
+
+    Аргументы:
+        utyazhelenie: осуществляет утяжеление режима
+        peretok_v_sechenii: определяет переток в сечении
+        kod: осуществляет расчёт режима
+    Возвращает:
+        Предельный переток в сечении.
+    """
+    while kod == 0:
+        kod = rastr.rgm('p')
+        for j in range(nodes_count):
+            if (zag_i.Z(j) * 1000) >= 100 and (zag_it.Z(j) * 1000) >= 100:
+                break
+        if (zag_i.Z(j) * 1000) < 100 or (zag_it.Z(j) * 1000) < 100:
+            utyazhelenie()
+            kod = rastr.rgm('p')
+        else:
+            peretok_v_sechenii()
+            break
+    return peretok_v_sechenii()
+
+
+# Определение предельного перетока по критерию обеспечения
+# допустимой токовой нагрузки линий электропередачи и
+# электросетевого оборудования в послеаварийных режимах
+# после нормативных возмущений.
+def pred_6(utyazhelenie, peretok_v_sechenii, kod):
+    """Функция осуществляет определение предельного перетока
+       по критерию обеспечения допустимой токовой нагрузки
+       линий электропередачи и электросетевого оборудования
+       в послеаварийных режимах после нормативных возмущений.
+
+    Аргументы:
+        utyazhelenie: осуществляет утяжеление режима
+        peretok_v_sechenii: определяет переток в сечении
+        kod: осуществляет расчёт режима
+    Возвращает:
+        Предельный переток в сечении.
+    """
+    pred = {}
+    index_vozmush_tok = list(index_vozmush_f())
+    listic_sta_faults_tok = df_faults.sta.T.tolist()
+
+    for j in range(len(index_vozmush_tok)):
+        for (index, sta_faults) in zip(
+                index_vozmush_tok, listic_sta_faults_tok):
+            sta.SetZ(index, sta_faults)
+            break
+        index_vozmush_tok.remove(index)
+        listic_sta_faults_tok.remove(sta_faults)
+        while kod == 0:
+            kod = rastr.rgm('p')
+            for j in range(nodes_count):
+                if (zag_i_av.Z(j) * 1000) >= 100 and \
+                        (zag_it_av.Z(j) * 1000) >= 100:
+                    break
+            if (zag_i_av.Z(j) * 1000) < 100 or \
+                    (zag_it_av.Z(j) * 1000) < 100:
+                utyazhelenie()
+                kod = rastr.rgm('p')
+            else:
+                sta.SetZ(index, abs(sta_faults - 1))
+                kod = rastr.rgm('p')
+                pred[index] = peretok_v_sechenii()
+                peretok_v_sechenii()
+                break
+        else:
+            sta.SetZ(index, abs(sta_faults - 1))
+            kod = rastr.rgm('p')
+            pred[index] = peretok_v_sechenii()
+            peretok_v_sechenii()
+        while kod == 0:
+            kod = rastr.rgm('p')
+            if peretok_v_sechenii() > p_sech_nach_f() and \
+                    len(index_vozmush_tok) > 0:
+                obratnoe_utyazhelenie()
+                kod = rastr.rgm('p')
+            else:
+                break
+
+    for key, value in pred.items():
+        min_pred_index = min(pred, key=pred.get)
+    min_pred = pred[min_pred_index]
+    return min_pred
--- raschet_mdp_kod.py
+++ raschet_mdp_kod.py
@@ -1,240 +1,72 @@
 import function_mdp as fm
 import pandas as pd
-import win32com.client
-import sys
+
+# Определяем переток в сечении в исходном режиме.
+fm.p_sech_nach_f()
 
 # Величина амплитуды нерегулярных колебаний.
 p_nk = 30
 
-# Осуществляем расчёт режима.
-kod = fm.rastr.rgm('p')
-
 # МДП по критерию обеспечения нормативного коэффициента запаса
 # статической апериодической устойчивости по активной мощности
 # в контролируемом сечении в нормальной схеме.
-while kod == 0:
-    fm.utyazhelenie()
-    kod = fm.rastr.rgm('p')
-    fm.peretok_v_sechenii()
-
-p_pred1 = fm.peretok_v_sechenii()
+p_pred1 = fm.pred_1(fm.utyazhelenie, fm.peretok_v_sechenii, fm.kod)
 p_mdp1 = 0.8 * p_pred1 - p_nk
 
+
 fm.vozvrat_k_ishodnomu_regimu()
-kod = fm.rastr.rgm('p')
+fm.rastr.rgm('p')
+
 
 # МДП по критерию обеспечения нормативного коэффициента запаса
 # статической устойчивости по напряжению
 # в узлах нагрузки в нормальной схеме.
-while kod == 0:
-    kod = fm.rastr.rgm('p')
-    for j in range(fm.nodes_count):
-        if fm.uras.Z(j) < (fm.unom.Z(j) * 0.7) * 1.15:
-            break
-    if fm.uras.Z(j) > (fm.unom.Z(j) * 0.7) * 1.15:
-        fm.utyazhelenie()
-        kod = fm.rastr.rgm('p')
-    else:
-        fm.peretok_v_sechenii()
-        break
-else:
-    fm.peretok_v_sechenii()
-
-p_pred2 = fm.peretok_v_sechenii()
+p_pred2 = fm.pred_2(fm.utyazhelenie, fm.peretok_v_sechenii, fm.kod)
 p_mdp2 = p_pred2 - p_nk
+
 
 fm.vozvrat_k_ishodnomu_regimu()
 kod = fm.rastr.rgm('p')
+
 
 # МДП по критерию обеспечения нормативного коэффициента запаса
 # статической апериодической устойчивости по активной мощности
 # в контролируемом сечении в послеаварийных режимах
 # после нормативных возмущений.
-pred = {}
-pred_sta = {}
-index_vozmush_statica = list(fm.index_vozmush)
-listic_sta_faults_statica = fm.df_faults.sta.T.tolist()
-
-for j in range(len(index_vozmush_statica)):
-    for (index, sta_faults) in zip(
-         index_vozmush_statica, listic_sta_faults_statica):
-        fm.sta.SetZ(index, sta_faults)
-        break
-    index_vozmush_statica.remove(index)
-    listic_sta_faults_statica.remove(sta_faults)
-    while kod == 0:
-        kod = fm.rastr.rgm('p')
-        if kod == 0:
-            fm.utyazhelenie()
-        else:
-            pred[index] = fm.peretok_v_sechenii() * 0.92
-            pred_sta[sta_faults] = fm.peretok_v_sechenii() * 0.92
-            fm.sta.SetZ(index, abs(sta_faults - 1))
-            kod = fm.rastr.rgm('p')
-            break
-    while kod == 0:
-        kod = fm.rastr.rgm('p')
-        if fm.peretok_v_sechenii() > fm.p_sech_nach and \
-                len(index_vozmush_statica) > 0:
-            fm.obratnoe_utyazhelenie()
-            kod = fm.rastr.rgm('p')
-            summa = 0
-        else:
-            break
-
-for key, value in pred.items():
-    min_pred_index = min(pred, key=pred.get)
-min_pred = pred[min_pred_index]
-
-for key, value in pred_sta.items():
-    min_pred_sta = min(pred_sta, key=pred_sta.get)
-
-# Повторное разутяжеление до наименьшего
-# предельного перетока с 8% запасом
-# из рассматриваемых возмущений
-kod = fm.rastr.rgm('p')
-while kod == 0:
-    for i in range(fm.nodes_count):
-        fm.sta.SetZ(min_pred_index, min_pred_sta)
-        break
-    kod = fm.rastr.rgm('p')
-    if fm.peretok_v_sechenii() > min_pred:
-        fm.obratnoe_utyazhelenie()
-        kod = fm.rastr.rgm('p')
-    else:
-        fm.sta.SetZ(min_pred_index, abs(min_pred_sta - 1))
-        fm.rastr.rgm('p')
-        fm.peretok_v_sechenii()
-        break
-
-p_pred3 = fm.peretok_v_sechenii()
+p_pred3 = fm.pred_3(fm.utyazhelenie, fm.peretok_v_sechenii, fm.kod)
 p_mdp3 = p_pred3 - p_nk
+
 
 fm.vozvrat_k_ishodnomu_regimu()
 kod = fm.rastr.rgm('p')
+
 
 # МДП по критерию обеспечения нормативного коэффициента запаса
 # статической устойчивости по напряжению в узлах нагрузки
 # в послеаварийных режимах после нормативных возмущений.
-pred = {}
-index_vozmush_naprygenie = list(fm.index_vozmush)
-listic_sta_faults_naprygenie = fm.df_faults.sta.T.tolist()
-kod = fm.rastr.rgm('p')
-
-for j in range(len(index_vozmush_naprygenie)):
-    for (index, sta_faults) in zip(
-            index_vozmush_naprygenie, listic_sta_faults_naprygenie):
-        fm.sta.SetZ(index, sta_faults)
-        break
-    index_vozmush_naprygenie.remove(index)
-    listic_sta_faults_naprygenie.remove(sta_faults)
-    while kod == 0:
-        kod = fm.rastr.rgm('p')
-        for j in range(fm.nodes_count):
-            if fm.uras.Z(j) < (fm.unom.Z(j) * 0.7) * 1.1:
-                break
-        if fm.uras.Z(j) > (fm.unom.Z(j) * 0.7) * 1.1:
-            fm.utyazhelenie()
-            kod = fm.rastr.rgm('p')
-        else:
-            fm.sta.SetZ(index, abs(sta_faults - 1))
-            kod = fm.rastr.rgm('p')
-            pred[index] = fm.peretok_v_sechenii()
-            fm.peretok_v_sechenii()
-            break
-    else:
-        fm.sta.SetZ(index, abs(sta_faults - 1))
-        kod = fm.rastr.rgm('p')
-        pred[index] = fm.peretok_v_sechenii()
-        fm.peretok_v_sechenii()
-    while kod == 0:
-        kod = fm.rastr.rgm('p')
-        if fm.peretok_v_sechenii() > fm.p_sech_nach and \
-                len(index_vozmush_naprygenie) > 0:
-            fm.obratnoe_utyazhelenie()
-            kod = fm.rastr.rgm('p')
-        else:
-            break
-
-for key, value in pred.items():
-    min_pred_index = min(pred, key=pred.get)
-min_pred = pred[min_pred_index]
-
-p_pred4 = min_pred
+p_pred4 = fm.pred_4(fm.utyazhelenie, fm.peretok_v_sechenii, fm.kod)
 p_mdp4 = p_pred4 - p_nk
+
 
 fm.vozvrat_k_ishodnomu_regimu()
 kod = fm.rastr.rgm('p')
+
 
 # МДП по критерию обеспечения допустимой токовой нагрузки
 # линий электропередачи и электросетевого оборудования
 # в нормальной схеме.
-while kod == 0:
-    kod = fm.rastr.rgm('p')
-    for j in range(fm.nodes_count):
-        if (fm.zag_i.Z(j) * 1000) >= 100 and (fm.zag_it.Z(j) * 1000) >= 100:
-            break
-    if (fm.zag_i.Z(j) * 1000) < 100 or (fm.zag_it.Z(j) * 1000) < 100:
-        fm.utyazhelenie()
-        kod = fm.rastr.rgm('p')
-    else:
-        fm.peretok_v_sechenii()
-        break
-
-p_pred5 = fm.peretok_v_sechenii()
+p_pred5 = fm.pred_5(fm.utyazhelenie, fm.peretok_v_sechenii, fm.kod)
 p_mdp5 = p_pred5 - p_nk
+
 
 fm.vozvrat_k_ishodnomu_regimu()
 kod = fm.rastr.rgm('p')
 
+
 # МДП по критерию обеспечения допустимой токовой нагрузки
 # линий электропередачи и электросетевого оборудования
 # в послеаварийных режимах после нормативных возмущений.
-pred = {}
-index_vozmush_tok = list(fm.index_vozmush)
-listic_sta_faults_tok = fm.df_faults.sta.T.tolist()
-
-for j in range(len(index_vozmush_tok)):
-    for (index, sta_faults) in zip(index_vozmush_tok, listic_sta_faults_tok):
-        fm.sta.SetZ(index, sta_faults)
-        break
-    index_vozmush_tok.remove(index)
-    listic_sta_faults_tok.remove(sta_faults)
-    while kod == 0:
-        kod = fm.rastr.rgm('p')
-        for j in range(fm.nodes_count):
-            if (fm.zag_i_av.Z(j) * 1000) >= 100 and \
-               (fm.zag_it_av.Z(j) * 1000) >= 100:
-                break
-        if (fm.zag_i_av.Z(j) * 1000) < 100 or \
-                (fm.zag_it_av.Z(j) * 1000) < 100:
-            fm.utyazhelenie()
-            kod = fm.rastr.rgm('p')
-        else:
-            fm.sta.SetZ(index, abs(sta_faults - 1))
-            kod = fm.rastr.rgm('p')
-            pred[index] = fm.peretok_v_sechenii()
-            fm.peretok_v_sechenii()
-            break
-    else:
-        fm.sta.SetZ(index, abs(sta_faults - 1))
-        kod = fm.rastr.rgm('p')
-        pred[index] = fm.peretok_v_sechenii()
-        fm.peretok_v_sechenii()
-    while kod == 0:
-        kod = fm.rastr.rgm('p')
-        if fm.peretok_v_sechenii() > fm.p_sech_nach and \
-                len(index_vozmush_tok) > 0:
-            fm.obratnoe_utyazhelenie()
-            kod = fm.rastr.rgm('p')
-        else:
-            break
-
-for key, value in pred.items():
-    min_pred_index = min(pred, key=pred.get)
-min_pred = pred[min_pred_index]
-
-p_pred6 = min_pred
+p_pred6 = fm.pred_6(fm.utyazhelenie, fm.peretok_v_sechenii, fm.kod)
 p_mdp6 = p_pred6 - p_nk
 
 
